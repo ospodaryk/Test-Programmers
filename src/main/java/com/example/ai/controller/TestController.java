3a649ccc618bc572ae8b9d2d54e1d29979dc7fd1
ospodaryk <ospodaryk11@gmail.com>
2023-03-07 17:06:45 +0200

    Added chart bar to all parts

--- src/main/java/com/example/ai/controller/TestController.java
+++ src/main/java/com/example/ai/controller/TestController.java
@@ -17,96 +17,11 @@ import java.util.Map;
 @Controller
 public class TestController {
     private User useer = new User("Taras");
-    private List<Question> p1questions = Arrays.asList(
-            new Question("Переживаєте за успіх в роботі", Arrays.asList(
-                    new Option("сильно", 5),
-                    new Option("не дуже", 3),
-                    new Option("спокійний", 2)
-            )),
-            new Question("Прагнете досягти швидко результату", Arrays.asList(
-                    new Option("поступово", 2),
-                    new Option("якомога швидше", 3),
-                    new Option("дуже", 5)
-            )),
-            new Question("Легко попадаєте в тупик при проблемах в роботі", Arrays.asList(
-                    new Option("неодмінно", 5),
-                    new Option("поступово", 3),
-                    new Option("зрідка", 2)
-            )),
-            new Question("Чи   потрібен чіткий алгоритм для вирішення задач", Arrays.asList(
-                    new Option("так", 5),
-                    new Option("окремих випадках", 3),
-                    new Option("не потрібен", 2)
-            ))
-    );
-    private List<Question> p2questions = Arrays.asList(
-            new Question("Чи використовуєте власний досвід при вирішенні задач?", Arrays.asList(
-                    new Option("зрідка", 5),
-                    new Option("частково", 3),
-                    new Option("ні", 2)
-            )),
-            new Question("Чи користуєтесь фіксованими правилами  для вирішення задач?", Arrays.asList(
-                    new Option("так", 2),
-                    new Option("в окремих випадках", 3),
-                    new Option("не потрібні", 5)
-            )),
-            new Question("Чи відчуваєте ви загальний контекст вирішення задачі?", Arrays.asList(
-                    new Option("в окремих випадках", 5),
-                    new Option("частково", 3),
-                    new Option("так", 2)
-            ))
-    );
-    private List<Question> p3questions = Arrays.asList(
-            new Question("Чи можете ви побудувати модель вирішуваної задачі?", Arrays.asList(
-                    new Option("так", 5),
-                    new Option("не повністю", 3),
-                    new Option("в окремих випадках", 2)
-            )),
-            new Question("Чи вистачає вам ініціативи при вирішенні задач?", Arrays.asList(
-                    new Option("так", 5),
-                    new Option("зрідка", 3),
-                    new Option("потрібне натхнення", 2)
-            )),
-            new Question("Чи можете вирішувати проблеми, з якими ще не стикались?", Arrays.asList(
-                    new Option("в окремих випадках", 3),
-                    new Option("ні", 5),
-                    new Option("так", 2)
-            ))
-    );
-    private List<Question> p4questions = Arrays.asList(
-            new Question("Чи  необхідний вам весь контекст задачі?", Arrays.asList(
-                    new Option("так", 5),
-                    new Option("в загальному", 2),
-                    new Option("в окремих випадках", 3)
-            )),
-            new Question("Чи переглядаєте ви свої наміри до вирішення задачі?", Arrays.asList(
-                    new Option("так", 5),
-                    new Option("зрідка", 3),
-                    new Option("коли є потреба", 2)
-            )),
-            new Question("Чи здатні  ви  навчатись у інших?", Arrays.asList(
-                    new Option("коли є потреба", 2),
-                    new Option("зрідка", 3),
-                    new Option("так", 5)
-            ))
-    );
-    private List<Question> p5questions = Arrays.asList(
-            new Question("Чи обираєте ви нові методи своєї роботи?", Arrays.asList(
-                    new Option("так", 5),
-                    new Option("вибірково", 3),
-                    new Option("при емоційному напруженні", 2)
-            )),
-            new Question("Чи допомагає власна інтуїція при вирішенні задач?", Arrays.asList(
-                    new Option("так", 5),
-                    new Option("зрідка", 3),
-                    new Option("коли є потреба", 2)
-            )),
-            new Question("Чи застовуєте рішення задач за аналогією?", Arrays.asList(
-                    new Option("часто", 5),
-                    new Option("зрідка", 3),
-                    new Option("тільки власний варіант", 2)
-            ))
-    );
+    private List<Question> p1questions = Arrays.asList(new Question("Переживаєте за успіх в роботі", Arrays.asList(new Option("сильно", 5), new Option("не дуже", 3), new Option("спокійний", 2))), new Question("Прагнете досягти швидко результату", Arrays.asList(new Option("поступово", 2), new Option("якомога швидше", 3), new Option("дуже", 5))), new Question("Легко попадаєте в тупик при проблемах в роботі", Arrays.asList(new Option("неодмінно", 5), new Option("поступово", 3), new Option("зрідка", 2))), new Question("Чи   потрібен чіткий алгоритм для вирішення задач", Arrays.asList(new Option("так", 5), new Option("окремих випадках", 3), new Option("не потрібен", 2))));
+    private List<Question> p2questions = Arrays.asList(new Question("Чи використовуєте власний досвід при вирішенні задач", Arrays.asList(new Option("зрідка", 5), new Option("частково", 3), new Option("ні", 2))), new Question("Чи користуєтесь фіксованими правилами  для вирішення задач", Arrays.asList(new Option("так", 2), new Option("в окремих випадках", 3), new Option("не потрібні", 5))), new Question("Чи відчуваєте ви загальний контекст вирішення задачі", Arrays.asList(new Option("в окремих випадках", 5), new Option("частково", 3), new Option("так", 2))));
+    private List<Question> p3questions = Arrays.asList(new Question("Чи можете ви побудувати модель вирішуваної задачі", Arrays.asList(new Option("так", 5), new Option("не повністю", 3), new Option("в окремих випадках", 2))), new Question("Чи вистачає вам ініціативи при вирішенні задач", Arrays.asList(new Option("так", 5), new Option("зрідка", 3), new Option("потрібне натхнення", 2))), new Question("Чи можете вирішувати проблеми, з якими ще не стикались", Arrays.asList(new Option("в окремих випадках", 3), new Option("ні", 5), new Option("так", 2))));
+    private List<Question> p4questions = Arrays.asList(new Question("Чи  необхідний вам весь контекст задачі", Arrays.asList(new Option("так", 5), new Option("в загальному", 2), new Option("в окремих випадках", 3))), new Question("Чи переглядаєте ви свої наміри до вирішення задачі", Arrays.asList(new Option("так", 5), new Option("зрідка", 3), new Option("коли є потреба", 2))), new Question("Чи здатні  ви  навчатись у інших", Arrays.asList(new Option("коли є потреба", 2), new Option("зрідка", 3), new Option("так", 5))));
+    private List<Question> p5questions = Arrays.asList(new Question("Чи обираєте ви нові методи своєї роботи", Arrays.asList(new Option("так", 5), new Option("вибірково", 3), new Option("при емоційному напруженні", 2))), new Question("Чи допомагає власна інтуїція при вирішенні задач", Arrays.asList(new Option("так", 5), new Option("зрідка", 3), new Option("коли є потреба", 2))), new Question("Чи застовуєте рішення задач за аналогією", Arrays.asList(new Option("часто", 5), new Option("зрідка", 3), new Option("тільки власний варіант", 2))));
 
     @GetMapping("/novice")
     public String getTest(Model model) {
@@ -116,8 +31,8 @@ public class TestController {
 
     @PostMapping("/novice/results")
     public String getResults(@RequestParam Map<String, String> answers, Model model) {
-        findResult(answers, model, 0);
-        model.addAttribute("questions", p1questions);
+        findResult(answers, model, 0, p1questions);
+
 
         return "results_novice";
     }
@@ -130,7 +45,7 @@ public class TestController {
 
     @PostMapping("/beginner/results")
     public String getResults2(@RequestParam Map<String, String> answers, Model model) {
-        findResult(answers, model, 1);
+        findResult(answers, model, 1, p2questions);
         return "results_beginner";
     }
 
@@ -142,7 +57,7 @@ public class TestController {
 
     @PostMapping("/competent/results")
     public String getResults3(@RequestParam Map<String, String> answers, Model model) {
-        findResult(answers, model, 2);
+        findResult(answers, model, 2, p3questions);
         return "results_competent";
     }
 
@@ -155,7 +70,7 @@ public class TestController {
 
     @PostMapping("/proficient/results")
     public String getResults4(@RequestParam Map<String, String> answers, Model model) {
-        findResult(answers, model, 3);
+        findResult(answers, model, 3, p4questions);
         return "results_proficient";
     }
 
@@ -167,16 +82,16 @@ public class TestController {
 
     @PostMapping("/expert/results")
     public String getResults5(@RequestParam Map<String, String> answers, Model model) {
-        findResult(answers, model, 4);
+        findResult(answers, model, 4, p5questions);
         return "results_expert";
     }
 
-    private void findResult(Map<String, String> answers, Model model, int part) {
+    private void findResult(Map<String, String> answers, Model model, int part, List<Question> list) {
         int score = 0;
-        int i=0;
+        int i = 0;
         for (Map.Entry<String, String> entry : answers.entrySet()) {
             score += Integer.parseInt(entry.getValue());
-            p1questions.get(i).setResult(Integer.parseInt(entry.getValue()));
+            list.get(i).setResult(Integer.parseInt(entry.getValue()));
             i++;
         }
         try {
@@ -184,6 +99,7 @@ public class TestController {
         } catch (Exception e) {
             useer.addMark(score);
         }
+        model.addAttribute("questions", list);
         model.addAttribute("user", useer);
     }
 
